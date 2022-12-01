58e16e258f6d2e705fc5fd8185e52ba97c9e4e29
terence-lim <41972012+terence-lim@users.noreply.github.com>
2022-12-01 18:13:16 -0500

    simplify

--- adjustmortality.py
+++ adjustmortality.py
@@ -12,32 +12,33 @@ import math
 class Adjust(Actuarial):
     """Adjust: adjusts mortality by extra risk
     
-    - life (Survival) : original survival and mortality functions
+    - life (Survival) : original survival and mortality rates
+    - adjust (int) : {ADD_FORCE, MULTIPLY_FORCE, ADD_AGE, MULTIPLY_RATE}
+    - extra (float) : amount of extra risk to adjust by
     """
     ADD_FORCE = 1
     MULTIPLY_FORCE = 2
     ADD_AGE = 3
     MULTIPLY_RATE = 4
-    _help = ['__getitem__', '__call__', 'q_x', 'p_x']
+    _help = ['q_x', 'p_x', 'q', 'p']
 
-    def __init__(self, life: Survival):
+    def __init__(self, life: Survival, adjust: int = 0, extra: float = 0.) -> "Adjust":
+        """Specify type and amount of mortality adjustment to apply"""
         self.life = life
-
-    def __call__(self, extra: float, adjust: int = 0) -> "Adjust":
-        """Specify type and amount of mortality adjustment to apply
-        - extra (float) : amount to adjust by
-        - adjust (int) : one of {ADD_FORCE, MULTIPLY_FORCE, ADD_AGE, MULTIPLY_RATE}
-        """
         self.extra = extra
         self.adjust = adjust
-        return self
 
-    def __getitem__(self, col: str) -> Dict[int, float]:
-        """Return adjusted survival or mortality values, as dict keyed by age
-        - col (str) : one of {'q', 'p'}
+    @property
+    def q(self) -> Dict[int, float]:
+        """Adjusted mortality rates q_x, as dict keyed by age
         """
-        fn = {'q': self.q_x, 'p': self.p_x}.get(col)
-        return {x: fn(x) for x in range(self.life.MINAGE, self.life.MAXAGE+1)}
+        return {x: self.q_x(x) for x in range(self.life.MINAGE, self.life.MAXAGE+1)}
+    
+    @property
+    def p(self) -> Dict[int, float]:
+        """Adjusted survival probabilities p_x, as dict keyed by age
+        """
+        return {x: self.p_x(x) for x in range(self.life.MINAGE, self.life.MAXAGE+1)}
 
     def p_x(self, x: int, s: int = 0) -> float:
         """Return p_[x]+s after adding or multiplying force of mortality
@@ -73,27 +74,25 @@ if __name__ == "__main__":
     
     print("SOA Question 5.5: (A) 1699.6")
     life = SULT()
-    adjust = Adjust(life=life)
-    q = adjust(extra=0.05, adjust=Adjust.ADD_FORCE)['q']
+    adjust = Adjust(life=life, extra=0.05, adjust=Adjust.ADD_FORCE)
     select = Select(n=1)\
-             .set_select(column=0, select_age=True, q=q)\
+             .set_select(column=0, select_age=True, q=adjust.q)\
              .set_select(column=1, select_age=False, a=life['a']).fill()
     print(100*select['a'][45][0])
     print()
 
     print("SOA Question 4.19:  (B) 59050")
     life = SULT()
-    adjust = Adjust(life=life)
-    q = adjust(extra=0.8, adjust=Adjust.MULTIPLY_RATE)['q']
+    adjust = Adjust(life=life, extra=0.8, adjust=Adjust.MULTIPLY_RATE)
     select = Select(n=1)\
-             .set_select(column=0, select_age=True, q=q)\
+             .set_select(column=0, select_age=True, q=adjust.q)\
              .set_select(column=1, select_age=False, q=life['q']).fill()
     print(100000*select.whole_life_insurance(80, s=0))
     print()
     
     print("Other usage examples")
     life = SULT()
-    adjust = Adjust(life=life)(extra=0.05, adjust=Adjust.ADD_FORCE)
+    adjust = Adjust(life=life, extra=0.05, adjust=Adjust.ADD_FORCE)
     print(life.p_x(45), adjust.p_x(45))
 
     print(Adjust.help())
